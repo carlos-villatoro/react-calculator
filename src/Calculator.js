8e9572216c29148b030fa06dcb37545094295541
carlos-villatoro <futurebound.carlosv@gmail.com>
2022-06-09 11:54:34 -0500

    adds bonus

--- src/Calculator.js
+++ src/Calculator.js
@@ -4,21 +4,45 @@ export default class Calculator extends Component{
     state = {
         num1:'',
         num2:'',
-        sum:''
-      }
+        sum:'',
+        operator: '+'
+    }
 
-      setNum = (e)=>{
-        this.setState({ [e.target.name]: e.target.value })
-      }
+    setNum = (e)=>{
+    this.setState({ [e.target.name]: e.target.value })
+    }
 
-      handleCalculate = (e)=>{
-        e.preventDefault()
-        let value = parseInt(this.state.num1) + parseInt(this.state.num2)
-        this.setState(()=>{
-            return {sum: value}
-        })
+    handleCalculate = (e)=>{
+    e.preventDefault()
+    this.setState(prevState=>{
+        let result =0
+        // swtich case based on operator
+        switch(this.state.operator){
+            case '+':
+                result = Number(prevState.num1) + Number(prevState.num2)
+                break
+            case '-':
+                result = prevState.num1 - prevState.num2
+                break
+            case '*':
+                result = prevState.num1 * prevState.num2
+                break
+            case '/':
+                result = prevState.num1 / prevState.num2
+                break
+            default:
+                console.warn(`${this.state.operator} is not a kind opf math i've heard of`)
+                break
+        }
+        return {sum: result}
+    })}
 
-      }
+    handleOperatorChange = (e)=>{
+    console.log(e)
+    this.setState({operator: e.target.value})
+    }
+
+    
     render(){
         return(
             <div className="caclulator">
@@ -30,7 +54,16 @@ export default class Calculator extends Component{
                     value={this.state.num1}
                     onChange={this.setNum}
                 />
-                <span>+</span>
+
+                <select name='operator' 
+                onChange={this.handleOperatorChange}
+                >
+                    <option value='+'>+</option>
+                    <option value='-'>-</option>
+                    <option value='*'>*</option>
+                    <option value='/'>/</option>
+                </select>
+                
                 <input type="number" 
                     name="num2"
                     value={this.state.num2}
